65b1ba9ed7fc91a37c0f869439aba0ef1fc305b4
unknown <fabriziotrevino@gmail.com>
2022-12-12 20:46:27 -0600

    Added readme file and change the code

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,7 +1,7 @@
 
 // Assignment code here
 
-alert
+alert("Welcome to Fabrizio's Random password generator");
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -20,11 +20,11 @@ function writePassword() {
 function generatePassword(){
 
   //Dialog Boxes
-  var passwordPopup = prompt("Please confirm the lenght of the password, it should be between 8-128 characters.");
-  var lowerCasePopup = confirm("Do you want to include lowercase letters?");
-  var upperCasePopup = confirm("Do you want to include uppercase letters?")
-  var numericPopup = confirm("Do you want numeric values?");
-  var symbolsPopup = confirm("Do you want to inlclude symbols?")
+  var passwordBox = prompt("Please confirm the lenght of the password, it should be between 8-128 characters.");
+  var lowerCaseBox = confirm("Do you want to include lowercase letters?");
+  var upperCaseBox = confirm("Do you want to include uppercase letters?")
+  var numericBox = confirm("Do you want numeric values?");
+  var symbolsBox = confirm("Do you want to inlclude symbols?");
 
   //Variable arrays
   var miniumLenght = 0;
@@ -32,6 +32,8 @@ function generatePassword(){
   var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var symbolsArray = [" ", "!", "”", "#", "$", "%", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">","," , "]", "?", "@", "[", "^", " - ", "`", "{", "|", "}", "~","\\",]; 
+
+
 
 };
 
@@ -43,51 +45,7 @@ var randomPassword = " ";
 
 var charactersArray = [
  //Lowercase 
-  function getLowerCase() {
-    return lowerCaseArray(math.floor(math.random * upperCaseArray.lenght))
-    },
+      getLowerCase: function() {
+      return lowerCaseArray[math.floor(math.random() * lowerCaseArray.lenght )]
 
-// Uppercase
-    function getUpperCase() {
-    return upperCaseArray(math.floor(math.random * upperCaseArray.lenght))
-    },
-
-// Numeric
-    function getNumeric() {
-    return numericArray(math.floor(math.random * numericArray.lenght))
-      },
-// Symbols    
-     function  getSymbols() {
-     return symbolsArray(math.floor(math.random * symbolsArray.lenght))
-      },
-    ];
-
-    
-    // if statements
-
-  // Lowerrcase
-if (lowerCase === true) {
-  lowerCase = charactersArray.getLowerCase();
-  miniumLenght++;
-}
-  // Uppercase
-if (upperCase === true) {
-  upperCase = charactersArray.getUpperCase();
-  miniumLenght++;
-}
-  // Numeric
-if (upperNumeric === true) {
-  numeric = charactersArray.getNumeric();
-  miniumLenght++ ;
-
-  // Symbols  
-if (symbols === true) {
-    symbols = charactersArray.getSymbols();
-    miniumLenght++ ;
-}
-
-// Random characters loop
-
-
-
-};
+]
--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Password Generator Starter Code
+
+## Table of contents
+
+* [Introduction](#introduction)
+* [Requirements](#requirements)
+* [Before we get started](#before-we-get-started)
+* [Personal opinion](#personal-opinion)
+* [Credits](#credits)
+
+## Introduction
+
+Welcome to my random password generator, you will be able to create a password with the following characteristics:
+
+* Uppercases
+* Lowercases
+* Numbers
+* Symbols
+
+## Requirements
+
+* GitHub
+
+* Your preferred browser
+
+* VS code
+
+## Before we get started
+
+Be sure to have somewhere safe to keep the generated password in case that you will be using it in the future.
+
+## Personal opinion
+
+Security is important, that is the reason that we are working really hard to create this random password generator.
+
+## Deployed links
+
+* [Github Pages](https://fabri-tech.github.io/Password-generator-Fabrizio-Trevi-o/)
+
+* [Github Repository](https://github.com/Fabri-Tech?tab=repositories)
+
+## Credits
+
+:wolf::wolf: Fabrizio Treviño :wolf::wolf:
