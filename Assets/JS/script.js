b8bf6b940a535a179e38136b2b945443707a1a49
unknown <fabriziotrevino@gmail.com>
2022-12-12 20:01:09 -0600

    Added Pop ups

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,7 @@
+
 // Assignment code here
 
-alert("Welcome to Fabrizio's Random password generator");
+alert
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -9,49 +10,60 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password"); 
+
+  passwordText.value = password;
+
 }
+
+// Constants
 
 function generatePassword(){
-  var passwordLenght = 15;
+
+  //Dialog Boxes
+  var passwordPopup = prompt("Please confirm the lenght of the password, it should be between 8-128 characters.");
+  var lowerCasePopup = confirm("Do you want to include lowercase letters?");
+  var upperCasePopup = confirm("Do you want to include uppercase letters?")
+  var numericPopup = confirm("Do you want numeric values?");
+  var symbolsPopup = confirm("Do you want to inlclude symbols?")
+
+  //Variable arrays
   var miniumLenght = 0;
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var symbols = [" ", "!", "”", "#", "$", "%", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">","," , "]", "?", "@", "[", "^", " - ", "`", "{", "|", "}", "~","\\",]; 
-}
-passwordText.value = password;
+  var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var symbolsArray = [" ", "!", "”", "#", "$", "%", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">","," , "]", "?", "@", "[", "^", " - ", "`", "{", "|", "}", "~","\\",]; 
 
-
+};
 
 
 // Add event listener to generate button
  generateBtn.addEventListener("click", writePassword);
 
-var randomPassword = "",
+var randomPassword = " ";
 
 var charactersArray = [
  //Lowercase 
   function getLowerCase() {
-    return lowerCase(math.floor(math.random * upperCase.lenght))
+    return lowerCaseArray(math.floor(math.random * upperCaseArray.lenght))
     },
 
 // Uppercase
     function getUpperCase() {
-    return upperCase(math.floor(math.random * upperCase.lenght))
+    return upperCaseArray(math.floor(math.random * upperCaseArray.lenght))
     },
 
 // Numeric
     function getNumeric() {
-    return numeric(math.floor(math.random * numeric.lenght))
+    return numericArray(math.floor(math.random * numericArray.lenght))
       },
 // Symbols    
-     function  getSymbols(){
-     return symbols(math.floor(math.random * symbols.lenght))
+     function  getSymbols() {
+     return symbolsArray(math.floor(math.random * symbolsArray.lenght))
       },
     ];
 
     
-    // if cases
+    // if statements
 
   // Lowerrcase
 if (lowerCase === true) {
@@ -76,8 +88,6 @@ if (symbols === true) {
 
 // Random characters loop
 
-for (let index = 0; index < array.length; index++) {
-	const element = array[index];
-	
-	
-}
+
+
+};
