61081bf111ca0d6bb9cddf7c4f9f6ec95fc9b7c9
unknown <fabriziotrevino@gmail.com>
2022-12-13 18:15:00 -0600

    changed the code formula

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,8 +1,19 @@
 
 // Assignment code here
 
+//Password variables
+const selectedCharacters = {
+  lowerCaseArray: 'abcdefghijklmnopqrstuvwxyz',
+  upperCaseArray: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  numericArray: '0123456789',
+  symbolsArray: " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+};
+
+
 alert("Welcome to Fabrizio's Random password generator");
 // Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,40 +23,56 @@ function writePassword() {
   var passwordText = document.querySelector("#password"); 
 
   passwordText.value = password;
-
 }
 
-// Constants
-
-function generatePassword(){
-
-  //Dialog Boxes
-  var passwordBox = prompt("Please confirm the lenght of the password, it should be between 8-128 characters.");
-  var lowerCaseBox = confirm("Do you want to include lowercase letters?");
-  var upperCaseBox = confirm("Do you want to include uppercase letters?")
-  var numericBox = confirm("Do you want numeric values?");
-  var symbolsBox = confirm("Do you want to inlclude symbols?");
-
-  //Variable arrays
-  var miniumLenght = 0;
-  var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var symbolsArray = [" ", "!", "”", "#", "$", "%", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">","," , "]", "?", "@", "[", "^", " - ", "`", "{", "|", "}", "~","\\",]; 
+generateBtn.addEventListener("click", writePassword);
 
 
+function generatePassword() {
 
-};
+  var passChar = "";
+
+    //Dialog Boxes 
+ 
+  var lengthBox = prompt("Please confirm the length of the password, it should be between 8 to 128 characters.");
+
+  var lowerCaseArray = confirm("Do you want to include lowercase letters?");
+
+  var upperCaseArray = confirm("Do you want to include uppercase letters?")
 
 
-// Add event listener to generate button
- generateBtn.addEventListener("click", writePassword);
+  var numericArray = confirm("Do you want to include numeric values?");
 
-var randomPassword = " ";
+  
+  var symbolsArray = confirm("Do you want to inlclude symbols?");
 
-var charactersArray = [
- //Lowercase 
-      getLowerCase: function() {
-      return lowerCaseArray[math.floor(math.random() * lowerCaseArray.lenght )]
 
-]
+
+  newFunction();
+  
+  // Formula for the password generator
+  var password = "";
+  for (let i = 0; i < lengthBox; i++) {
+  password += passChar[Math.floor(Math.random() * passChar.length )]
+   }
+  return password
+
+//Conditional statements
+  function newFunction() {
+    if (lowerCaseArray) {
+      passChar += selectedCharacters.lowerCaseArray;
+    };
+
+    if (upperCaseArray) {
+      passChar += selectedCharacters.upperCaseArray;
+    };
+
+    if (numericArray) {
+      passChar += selectedCharacters.numericArray;
+    };
+
+    if (symbolsArray) {
+      passChar += selectedCharacters.symbolsArray;
+    };
+  }
+}
