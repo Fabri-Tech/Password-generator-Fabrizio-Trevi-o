06bcd472210fdfaab3ebe73ecdadde8dfd705921
Fabrizio Treviño <fabriziotrevino@gmail.com>
2023-03-16 19:35:05 -0600

    updated JS and readme

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -10,7 +10,6 @@ const selectedCharacters = {
 
 };
 
-
 alert("Welcome to Fabrizio's Random password generator");
 // Get references to the #generate element
 
@@ -25,39 +24,47 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// This event listener will make the GENERATE BUTTON work
 generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword() {
-
   var passChar = "";
 
     //Dialog Boxes 
+   var lengthBox = prompt("Please confirm the length of the password, it should be between 8 to 128 characters.");
+    
+  //Check if the entered length is within the specified range
+    if (lengthBox < 8 || lengthBox > 128) {
+      alert("Password length should be between 8 to 128 characters.");
+      return "";
+    }
+
  
-  var lengthBox = prompt("Please confirm the length of the password, it should be between 8 to 128 characters.");
 
-  var lowerCaseArray = confirm("Do you want to include lowercase letters?");
-
-  var upperCaseArray = confirm("Do you want to include uppercase letters?")
-
-
-  var numericArray = confirm("Do you want to include numeric values?");
-
-  
-  var symbolsArray = confirm("Do you want to inlclude symbols?");
+  // This section will make the user select which type of characters will be included in the code
+  let lowerCaseArray = confirm("Do you want to include lowercase letters?");
+  let upperCaseArray = confirm("Do you want to include uppercase letters?")
+  let numericArray = confirm("Do you want to include numeric values?");
+  let symbolsArray = confirm("Do you want to inlclude symbols?");
 
 
+// This IF statement section will make the user select at least 1 option
+  if (!lowerCaseArray && !upperCaseArray && !numericArray && !symbolsArray) {
+    alert("Please select at least one type of character to include in your password.");
+    return "";
+  }
 
   newFunction();
   
-  // Formula for the password generator
-  var password = "";
+  // This section will make the characters selected appear in random order
+  let password = "";
   for (let i = 0; i < lengthBox; i++) {
   password += passChar[Math.floor(Math.random() * passChar.length )]
    }
   return password
 
-//Conditional statements
+//Conditional statements for the password combinations
   function newFunction() {
     if (lowerCaseArray) {
       passChar += selectedCharacters.lowerCaseArray;
@@ -73,6 +80,6 @@ function generatePassword() {
 
     if (symbolsArray) {
       passChar += selectedCharacters.symbolsArray;
-    };
+    };  
   }
 }
--- README.md
+++ README.md
@@ -3,24 +3,54 @@
 ## Table of contents 🚀
 
 * [Introduction](#introduction)
-* [Requirements](#requirements-📣)
-* [Before we get started](#before-we-get-started-🚩)
-* [Personal opinion](#personal-opinion-™️)
+* [Requirements](#requirements)
+* [Before we get started](#before-we-get-started)
+* [Personal opinion](#personal-opinion)
+* [Deployed links](#deployed-links)
 * [Credits](#credits)
 
-## Introduction 💣
+## Introduction 
 
-Welcome to my random password generator, you will be able to create a password with the following characteristics:
+This is a simple random password generator that allows users to select the length of their password and the type of characters they want to include.
 
-* Uppercases 🆎
+## How to Use
+
+Click the "Generate Password" button to start the process.
+
+![homepage](./Assets/homepage.PNG)
+
+When prompted, enter the desired length of your password, which should be between 8 to 128 characters.
+
+![characters](./Assets/characters.PNG)
+
+You will then be prompted to select which type of characters you want to include in your password. You can choose to include the following:
 
 * Lowercases 🆒
 
+![lowercase](./Assets/lowercase.PNG)
+
+* Uppercases 🅰️
+
+![uppercase](./Assets/uppercase.PNG)
+
 * Numbers 🔢
+
+![numbers](./Assets/numbers.PNG)
 
 * Symbols 🉐
 
-## Requirements 📣
+![symbols](./Assets/symbols.PNG)
+
+At least one type of character must be selected.
+
+![options](./Assets/options.PNG)
+
+Once you have selected your desired options, your password will be generated and displayed in the "Your Secure Password" box.
+
+![password](./Assets/password.PNG)
+
+
+## Requirements 
 
 * GitHub 🐱
 
@@ -30,15 +60,15 @@ Welcome to my random password generator, you will be able to create a password w
 
 * A place to store your password. 🛡️
 
-## Before we get started 🚩
+## Before we get started
 
 Be sure to have somewhere safe to keep the generated password in case that you will be using it in the future.
 
-## Personal opinion :tm:
+## Personal opinion
 
 Security is important, that is the reason that we are working really hard to create this random password generator.
 
-## Deployed links ✏️
+## Deployed links
 
 * [GitHub Pages](https://fabri-tech.github.io/Password-generator-Fabrizio-Trevi-o/)
 
